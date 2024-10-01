f03cd894561272262b6fe127dbcc96bff3056aaa
Gustavo Santos <santos.gustavo.of@gmail.com>
2024-10-01 19:08:07 -0300

    Implementação das operações de Update e Delete no banco de dados

--- SistemaVetConsole/src/interfaces/dao/ClienteDAO.java
+++ SistemaVetConsole/src/interfaces/dao/ClienteDAO.java
@@ -1,14 +1,15 @@
 package interfaces.dao;
 
+import java.io.IOException;
 import java.util.List;
 import entities.Cliente;
 import exceptions.DomainException;
 
 public interface ClienteDAO {
 	
-	void insert(Cliente cli);
+	void insert(Cliente cli) throws IOException;
 	void update(Cliente cli);
 	void deleteById(Integer id);
 	Cliente findById(Integer id) throws DomainException;
-	List<Cliente> findAll();
+	List<Cliente> findAll() throws DomainException;
 }
--- SistemaVetConsole/src/interfaces/dao/impl/ClienteDaoJDBC.java
+++ SistemaVetConsole/src/interfaces/dao/impl/ClienteDaoJDBC.java
@@ -1,5 +1,10 @@
 package interfaces.dao.impl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,6 +12,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,9 +21,6 @@ import db.DB;
 import db.DbException;
 import entities.Animal;
 import entities.Cliente;
-import enums.FormaPagamento;
-import enums.Situacao;
-import enums.StatusPagamento;
 import exceptions.DomainException;
 import interfaces.dao.ClienteDAO;
 
@@ -31,13 +35,14 @@ public class ClienteDaoJDBC implements ClienteDAO {
 	}
 	
 	@Override
-	public void insert(Cliente cli) {
+	public void insert(Cliente cli) throws IOException {
 		PreparedStatement st = null;
 		ResultSet rs = null; 
 		
 		int id = 0;
 		
 		try {
+			conn.setAutoCommit(false);
 			
 			st = conn.prepareStatement(
 					"INSERT INTO cliente (nome, cpfCnpj, telefone, qtdAnimal, orcamentoTotal,"
@@ -62,19 +67,88 @@ public class ClienteDaoJDBC implements ClienteDAO {
 				
 				while(rs.next()) {
 					id = rs.getInt(1);
+					cli.setId(id);
 				}
+			}
+			
+			else {
+				conn.rollback();
+				throw new DbException("Erro Inesperado. Nenhuma linha adicionada.");
 			}
 			
 			for(int i = 0; i < cli.qtdAnimal; i++) {
 				st = conn.prepareStatement(
-						"INSERT INTO animal (idCliente, nome, sexo, tipo, orcamento) "
-						+ "VALUES (?, ? ,?, ?, ?)");
+						"INSERT INTO animal (idCliente, nome, sexo, tipo, orcamento, servicos ) "
+						+ "VALUES (?, ? ,?, ?, ?, ?)");
 				
 				st.setInt	(1, id);							//=> IdCliente
 				st.setString(2, cli.animal[i].getNome());		//=> Nome Pet
 				st.setInt	(3, cli.animal[i].getIntSexo());	//=> Sexo Pet
 				st.setInt	(4, cli.animal[i].getIntTipo());	//=> TipoPet
 				st.setDouble(5, cli.animal[i].getOrcamento());	//=> Orçamento pet
+				
+				String intString = Arrays.toString(cli.animal[i].getServicos());
+				st.setString(6, intString);
+				
+				st.executeUpdate();
+				conn.commit();
+			}
+		}
+		catch(SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				
+				System.out.println("Erro: Não foi possível realizar o RollBack.\n Stacktrace: " + e1.getMessage());
+			}
+			throw new DbException(e.getMessage());
+		}
+		finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
+	}
+
+	@Override
+	public void update(Cliente cli) {
+		PreparedStatement st = null;
+		ResultSet rs = null; 
+		
+		try {
+			
+			conn.setAutoCommit(true);
+			st = conn.prepareStatement(
+					"UPDATE cliente SET nome = ?, cpfCnpj = ?, telefone = ?, qtdAnimal = ?, orcamentoTotal = ?,"
+					+ " formaPagamento = ?, parcelas = ?, statusPagamento = ?, situacao = ?, dataCadastro = ?"
+					+ "WHERE id = ?");
+			
+			st.setString(1, cli.getNome()); 			//=> Nome
+			st.setString(2, cli.getCpf()); 				//=> Cpf/Cnpj
+			st.setString(3, cli.getTelefone()); 		//=> Telefone
+			st.setInt(4, cli.qtdAnimal); 	   			//=> QtdAnimal
+			st.setDouble(5, cli.getOrcamento());	   	//=> OrçamentoTotal
+			st.setInt(6, cli.getIntFormaPagamento());	//=> Forma de Pagamento
+			st.setInt(7, cli.parcelaPagamento);			//=> Parcelas
+			st.setInt(8, cli.getIntStatusPagamento());	//=> Status do Pagamento
+			st.setInt(9, cli.getIntSituacao());			//=> Situação do Trabalho
+			st.setTimestamp(10, Timestamp.valueOf(cli.getDataCadastro())); //=> Data de Cadastro
+			st.setInt(11, cli.getId()); 				//=> Id do Cliente
+			
+			st.executeUpdate();
+		
+			
+			for(int i = 0; i < cli.qtdAnimal; i++) {
+				st = conn.prepareStatement(
+						"UPDATE animal SET idCliente = ?, nome = ?, sexo = ?, tipo = ?, orcamento = ? "
+						+ "WHERE idCliente = ?");
+				
+				st.setInt	(1, cli.getId());					//=> IdCliente
+				st.setString(2, cli.animal[i].getNome());		//=> Nome Pet
+				st.setInt	(3, cli.animal[i].getIntSexo());	//=> Sexo Pet
+				st.setInt	(4, cli.animal[i].getIntTipo());	//=> TipoPet
+				st.setDouble(5, cli.animal[i].getOrcamento());	//=> Orçamento pet
+				st.setInt	(6, cli.getId());					//=> IdCliente
 				
 				st.executeUpdate();
 			}
@@ -90,14 +164,28 @@ public class ClienteDaoJDBC implements ClienteDAO {
 	}
 
 	@Override
-	public void update(Cliente cli) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn.setAutoCommit(true);
+			st = conn.prepareStatement("DELETE FROM cliente WHERE id = ?");
+			st.setInt(1, id);
+			st.executeUpdate();
+			
+			st = conn.prepareStatement("DELETE FROM animal WHERE idCliente = ?");
+			st.setInt(1, id);
+			st.executeUpdate();
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 		
 	}
 
@@ -108,6 +196,7 @@ public class ClienteDaoJDBC implements ClienteDAO {
 		ResultSet rs = null; 
 		
 		try {
+			conn.setAutoCommit(true);
 			st = conn.prepareStatement(
 					"SELECT cliente.*, animal.* "
 					+ "FROM cliente INNER JOIN animal "
@@ -145,6 +234,58 @@ public class ClienteDaoJDBC implements ClienteDAO {
 		}
 	}
 	
+	@Override
+	public List<Cliente> findAll() throws DomainException {
+		PreparedStatement st = null;
+		ResultSet rs = null; 
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT cliente.*, animal.* "
+					+ "FROM cliente INNER JOIN animal "
+					+ "ON cliente.id = animal.idCliente "
+					+ "ORDER BY dataCadastro");
+			
+			rs = st.executeQuery();
+			
+			List<Cliente> listaClientes = new ArrayList<Cliente>();
+			int rows = 0;
+			
+			while(rs.next()) {
+				
+				Cliente cliente = instCliente(rs);	
+				rows++;
+				
+				for(int i = 0; i < cliente.qtdAnimal; i++) {
+					cliente.animal[i] = instAnimal(rs, cliente, i);
+					
+					if(i == cliente.qtdAnimal - 1) {
+						continue;
+					}
+					
+					if(!rs.next()) {
+						break;
+					}
+					
+
+				}
+				
+				listaClientes.add(cliente);
+			}
+			
+			System.out.println("Clientes Encontrados: " + rows + "\n");
+			return listaClientes;
+					
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
 	private Cliente instCliente(ResultSet rs) throws SQLException, DomainException {
 		Cliente cliente = new Cliente();
 		
@@ -170,14 +311,31 @@ public class ClienteDaoJDBC implements ClienteDAO {
 		animal.setSexo(rs.getInt("animal.sexo"));
 		animal.setTipo(rs.getInt("animal.tipo"));
 		animal.setOrcamento(rs.getDouble("animal.orcamento"));
+		String intStr = rs.getString("animal.servicos");
+		
+		intStr = intStr.replaceAll(" ", "");
+		intStr = intStr.substring(1, intStr.length() - 1);
+
+		String[] parts = intStr.split(",");
+		int[] arr = new int[parts.length];
+		
+		int j = 0;
+		for (String part : parts) {
+			
+		    part = part.trim();
+		    
+		    if (part.startsWith("-")) {
+		        arr[j] = -1;
+		        j++;
+		        continue;
+		    }
+		    
+		    arr[j] = Integer.parseInt(part);
+		    j++;
+		}
+		
+		animal.servicos = arr;
 		
 		return animal;
 	}
-
-	@Override
-	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 }
